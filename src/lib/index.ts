476ab328cae19cdb7c92e7931bedb838204d3606
kareyes <kas.reyes28@gmail.com>
2026-01-19 23:10:22 +0800

    feat: update version to 0.0.5 and enhance component exports in index.ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,48 +1,216 @@
-// place files you want to import through the `$lib` alias in this folder.
-// import * as Icon from "@lucide/svelte"
+// Aether UI Component Library
+// All components export both Primitives (namespace with all parts) and shorthand (Root component)
 
-import * as Accordion from "./components/ui/accordion/index.js";
-import * as Alert from "./components/ui/alert/index.js";
-import * as AlertDialog from "./components/ui/alert-dialog/index.js";
-import * as Avatar from "./components/ui/avatar/index.js";
-import * as Badge from "./components/ui/badge/index.js";
-import * as Breadcrumb from "./components/ui/breadcrumb/index.js";
-import * as Button from "./components/ui/button/index.js";
-import * as ButtonGroup from "./components/ui/button-group/index.js";
-import * as Calendar from "./components/ui/calendar/index.js";
-import * as Card from "./components/ui/card/index.js";
-import * as Checkbox from "./components/ui/checkbox/index.js";
-import * as ComboBox from "./components/ui/combobox/index.js";
-import * as Command from "./components/ui/command/index.js";
-import * as DataTable from "./components/ui/data-table/index.js";
-import * as DatePicker from "./components/ui/date-picker/index.js";
-import * as Dialog from "./components/ui/dialog/index.js";
-import * as DropdownMenu from "./components/ui/dropdown-menu/index.js";
-import * as Field from "./components/ui/field/index.js";
-import * as FileInput from "./components/ui/file-input/index.js";
-import * as Input from "./components/ui/input/index.js";
-import * as InputGroup from "./components/ui/input-group/index.js";
-import * as InputOTP from "./components/ui/input-otp/index.js";
-import * as Label from "./components/ui/label/index.js";
-import * as NumberSpinner from "./components/ui/number-spinner/index.js";
-import * as Popover from "./components/ui/popover/index.js";
-import * as RadioGroup from "./components/ui/radio/index.js";
-import * as RangeCalendar from "./components/ui/range-calendar/index.js";
-import * as Select from "./components/ui/select/index.js";
-import * as Separator from "./components/ui/separator/index.js";
-import * as Sheet from "./components/ui/sheet/index.js";
-import * as Sidebar from "./components/ui/sidebar/index.js";
-import * as Skeleton from "./components/ui/skeleton/index.js";
-import * as Slider from "./components/ui/slider/index.js";
-import * as Sonner from "./components/ui/sonner/index.js";
-import * as Spinner from "./components/ui/spinner/index.js";
-import * as Stepper from "./components/ui/stepper/index.js";
-import * as Switch from "./components/ui/switch/index.js";
-import * as Table from "./components/ui/table/index.js";
-import * as Tabs from "./components/ui/tabs/index.js";
-import * as Textarea from "./components/ui/textarea/index.js";
-import * as Tooltip from "./components/ui/tooltip/index.js";
+// Accordion
+import * as AccordionPrimitives from "./components/ui/accordion/index.js";
+import { Root as Accordion } from "./components/ui/accordion/index.js";
 
+// Alert
+import * as AlertPrimitives from "./components/ui/alert/index.js";
+import { Alert } from "./components/ui/alert/index.js";
+
+// AlertDialog
+import * as AlertDialogPrimitives from "./components/ui/alert-dialog/index.js";
+import { Root as AlertDialog } from "./components/ui/alert-dialog/index.js";
+
+// Avatar
+import * as AvatarPrimitives from "./components/ui/avatar/index.js";
+import { Avatar } from "./components/ui/avatar/index.js";
+
+// Badge
+import * as BadgePrimitives from "./components/ui/badge/index.js";
+import { Badge } from "./components/ui/badge/index.js";
+
+// Breadcrumb
+import * as BreadcrumbPrimitives from "./components/ui/breadcrumb/index.js";
+import { Root as Breadcrumb } from "./components/ui/breadcrumb/index.js";
+
+// Button
+import * as ButtonPrimitives from "./components/ui/button/index.js";
+import { Button } from "./components/ui/button/index.js";
+
+// ButtonGroup
+import * as ButtonGroupPrimitives from "./components/ui/button-group/index.js";
+import { ButtonGroup } from "./components/ui/button-group/index.js";
+
+// Calendar
+import * as CalendarPrimitives from "./components/ui/calendar/index.js";
+import { Calendar } from "./components/ui/calendar/index.js";
+
+// Card
+import * as CardPrimitives from "./components/ui/card/index.js";
+import { Card } from "./components/ui/card/index.js";
+
+// Checkbox
+import * as CheckboxPrimitives from "./components/ui/checkbox/index.js";
+import { Checkbox } from "./components/ui/checkbox/index.js";
+
+// ComboBox
+import * as ComboBoxPrimitives from "./components/ui/combobox/index.js";
+import { Combobox as ComboBox } from "./components/ui/combobox/index.js";
+
+// Command
+import * as CommandPrimitives from "./components/ui/command/index.js";
+import { Root as Command } from "./components/ui/command/index.js";
+
+// DataTable
+import * as DataTablePrimitives from "./components/ui/data-table/index.js";
+import { DataTable } from "./components/ui/data-table/index.js";
+
+// DatePicker
+import * as DatePickerPrimitives from "./components/ui/date-picker/index.js";
+import { DatePicker, DatePickerWithPresets, DateRangePicker } from "./components/ui/date-picker/index.js";
+
+// Dialog
+import * as DialogPrimitives from "./components/ui/dialog/index.js";
+import { Root as Dialog } from "./components/ui/dialog/index.js";
+
+// DropdownMenu
+import * as DropdownMenuPrimitives from "./components/ui/dropdown-menu/index.js";
+import { DropdownMenu } from "./components/ui/dropdown-menu/index.js";
+
+// Field
+import * as FieldPrimitives from "./components/ui/field/index.js";
+import { Field } from "./components/ui/field/index.js";
+
+// FileInput
+import * as FileInputPrimitives from "./components/ui/file-input/index.js";
+import { FileInput, FileInputButton, FileInputDragDrop, FileInputRegular } from "./components/ui/file-input/index.js";
+
+// Input
+import * as InputPrimitives from "./components/ui/input/index.js";
+import { Input } from "./components/ui/input/index.js";
+
+// InputGroup
+import * as InputGroupPrimitives from "./components/ui/input-group/index.js";
+import { Root as InputGroup } from "./components/ui/input-group/index.js";
+
+// InputOTP
+import * as InputOTPPrimitives from "./components/ui/input-otp/index.js";
+import { InputOTP } from "./components/ui/input-otp/index.js";
+
+// Label
+import * as LabelPrimitives from "./components/ui/label/index.js";
+import { Label } from "./components/ui/label/index.js";
+
+// NumberSpinner
+import * as NumberSpinnerPrimitives from "./components/ui/number-spinner/index.js";
+import { NumberSpinner } from "./components/ui/number-spinner/index.js";
+
+// Popover
+import * as PopoverPrimitives from "./components/ui/popover/index.js";
+import { Root as Popover } from "./components/ui/popover/index.js";
+
+// RadioGroup
+import * as RadioGroupPrimitives from "./components/ui/radio/index.js";
+import { RadioGroup } from "./components/ui/radio/index.js";
+
+// RangeCalendar
+import * as RangeCalendarPrimitives from "./components/ui/range-calendar/index.js";
+import { RangeCalendar } from "./components/ui/range-calendar/index.js";
+
+// Select
+import * as SelectPrimitives from "./components/ui/select/index.js";
+import {  Select } from "./components/ui/select/index.js";
+
+// Separator
+import * as SeparatorPrimitives from "./components/ui/separator/index.js";
+import { Separator } from "./components/ui/separator/index.js";
+
+// Sheet
+import * as SheetPrimitives from "./components/ui/sheet/index.js";
+import { Root as Sheet } from "./components/ui/sheet/index.js";
+
+// Sidebar
+import * as SidebarPrimitives from "./components/ui/sidebar/index.js";
+import { Root as Sidebar } from "./components/ui/sidebar/index.js";
+
+// Skeleton
+import * as SkeletonPrimitives from "./components/ui/skeleton/index.js";
+import { Skeleton } from "./components/ui/skeleton/index.js";
+
+// Slider
+import * as SliderPrimitives from "./components/ui/slider/index.js";
+import { Slider } from "./components/ui/slider/index.js";
+
+// Sonner
+import * as SonnerPrimitives from "./components/ui/sonner/index.js";
+import { Toaster as Sonner } from "./components/ui/sonner/index.js";
+
+// Spinner
+import * as SpinnerPrimitives from "./components/ui/spinner/index.js";
+import { Spinner } from "./components/ui/spinner/index.js";
+
+// Stepper
+import * as StepperPrimitives from "./components/ui/stepper/index.js";
+import { Root as Stepper } from "./components/ui/stepper/index.js";
+
+// Switch
+import * as SwitchPrimitives from "./components/ui/switch/index.js";
+import { Switch } from "./components/ui/switch/index.js";
+
+// Table
+import * as TablePrimitives from "./components/ui/table/index.js";
+import { Root as Table } from "./components/ui/table/index.js";
+
+// Tabs
+import * as TabsPrimitives from "./components/ui/tabs/index.js";
+import { Root as Tabs } from "./components/ui/tabs/index.js";
+
+// Textarea
+import * as TextareaPrimitives from "./components/ui/textarea/index.js";
+import { Textarea } from "./components/ui/textarea/index.js";
+
+// Tooltip
+import * as TooltipPrimitives from "./components/ui/tooltip/index.js";
+import { Root as Tooltip } from "./components/ui/tooltip/index.js";
+
+// Export all primitives (namespace exports with all component parts)
+export {
+	AccordionPrimitives,
+	AlertPrimitives,
+	AlertDialogPrimitives,
+	AvatarPrimitives,
+	BadgePrimitives,
+	BreadcrumbPrimitives,
+	ButtonPrimitives,
+	ButtonGroupPrimitives,
+	CalendarPrimitives,
+	CardPrimitives,
+	CheckboxPrimitives,
+	ComboBoxPrimitives,
+	CommandPrimitives,
+	DataTablePrimitives,
+	DatePickerPrimitives,
+	DialogPrimitives,
+	DropdownMenuPrimitives,
+	FieldPrimitives,
+	FileInputPrimitives,
+	InputPrimitives,
+	InputGroupPrimitives,
+	InputOTPPrimitives,
+	LabelPrimitives,
+	NumberSpinnerPrimitives,
+	PopoverPrimitives,
+	RadioGroupPrimitives,
+	RangeCalendarPrimitives,
+	SelectPrimitives,
+	SeparatorPrimitives,
+	SheetPrimitives,
+	SidebarPrimitives,
+	SkeletonPrimitives,
+	SliderPrimitives,
+	SonnerPrimitives,
+	SpinnerPrimitives,
+	StepperPrimitives,
+	SwitchPrimitives,
+	TablePrimitives,
+	TabsPrimitives,
+	TextareaPrimitives,
+	TooltipPrimitives,
+};
+
+// Export shorthand components (Root component of each)
 export {
 	Accordion,
 	Alert,
@@ -59,10 +227,15 @@ export {
 	Command,
 	DataTable,
 	DatePicker,
+	DatePickerWithPresets,
+	DateRangePicker,
 	Dialog,
 	DropdownMenu,
 	Field,
 	FileInput,
+	FileInputButton,
+	FileInputDragDrop,
+	FileInputRegular,
 	Input,
 	InputGroup,
 	InputOTP,
@@ -84,5 +257,5 @@ export {
 	Table,
 	Tabs,
 	Textarea,
-	Tooltip
+	Tooltip,
 };
